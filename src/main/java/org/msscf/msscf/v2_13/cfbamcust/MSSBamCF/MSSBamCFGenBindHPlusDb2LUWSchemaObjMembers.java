2b2cdac1fe2b491040265c64f113fa8979d450ff
Mark Sobkow <mark.sobkow@gmail.com>
2026-03-15 13:30:34 -0600

    2026-03-16 13:30 CST Clean up licensing, integrate resource resolution via mcf.markhome.server for URLs, and generally clean up the 2.12 and 2.13 code bases

--- src/main/java/org/msscf/msscf/v2_13/cfbamcust/MSSBamCF/MSSBamCFGenBindHPlusDb2LUWSchemaObjMembers.java
+++ src/main/java/org/msscf/msscf/v2_13/cfbamcust/MSSBamCF/MSSBamCFGenBindHPlusDb2LUWSchemaObjMembers.java
@@ -1,20 +1,40 @@
 /*
  *	MSS Code Factory CFBam 2.13 CustMSSBamCF
  *
- *	Copyright (C) 2016-2026 Mark Stephen Sobkow (mailto:mark.sobkow@gmail.com)
+ *	Copyright (c) 2016-2026 Mark Stephen Sobkow
  *	
- *	This program is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
+ *	Mark's Code Fractal CFBam 2.13 Business Application Model
+ *	
+ *	Copyright 2016-2026 Mark Stephen Sobkow
+ *	
+ *	This file is part of Mark's Code Fractal CFBam.
+ *	
+ *	Mark's Code Fractal CFBam is available under dual commercial license from
+ *	Mark Stephen Sobkow, or under the terms of the GNU General Public License,
+ *	Version 3 or later with classpath and static linking exceptions.
+ *	
+ *	As a special exception, Mark Sobkow gives you permission to link this library
+ *	with independent modules to produce an executable, provided that none of them
+ *	conflict with the intent of the GPLv3; that is, you are not allowed to invoke
+ *	the methods of this library from non-GPLv3-compatibly licensed code. You may not
+ *	implement an LPGLv3 "wedge" to try to bypass this restriction. That said, code which
+ *	does not rely on this library is free to specify whatever license its authors decide
+ *	to use. Mark Sobkow specifically rejects the infectious nature of the GPLv3, and
+ *	considers the mere act of including GPLv3 modules in an executable to be perfectly
+ *	reasonable given tools like modern Java's single-jar deployment options.
+ *	
+ *	Mark's Code Fractal CFBam is free software: you can redistribute it and/or
+ *	modify it under the terms of the GNU General Public License as published by
  *	the Free Software Foundation, either version 3 of the License, or
  *	(at your option) any later version.
  *	
- *	This program is distributed in the hope that it will be useful,
+ *	Mark's Code Fractal CFBam is distributed in the hope that it will be useful,
  *	but WITHOUT ANY WARRANTY; without even the implied warranty of
  *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *	GNU General Public License for more details.
  *	
  *	You should have received a copy of the GNU General Public License
- *	along with this program.  If not, see &lt;https://www.gnu.org/licenses/&gt;.
+ *	along with Mark's Code Fractal CFBam.  If not, see <https://www.gnu.org/licenses/>.
  *	
  *	If you wish to modify and use this code without publishing your changes,
  *	or integrate it with proprietary code, please contact Mark Stephen Sobkow
